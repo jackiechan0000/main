90b7bfcb63b614afbcf6c808da02241e7f7ffe20
Chan Meletio <88849562+jackiechan0000@users.noreply.github.com>
2025-04-21 22:44:53 -0500

    Update loadcell_arduino.ino

--- loadcell_arduino.ino
+++ loadcell_arduino.ino
@@ -1,6 +1,6 @@
 #include "HX711.h"
 
-// Download the HX711 library in the arduino library to get use the
+// Download HX711 library in arduino IDE to use the 
 // load cell booster
 
 // HX711 circuit wiring
@@ -12,34 +12,18 @@ HX711 scale;
 void setup() {
   Serial.begin(115200);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-
-  Serial.println("Initializing the scale...");
-  
-  // Wait for scale to be ready
-  while (!scale.is_ready()) {
-    Serial.println("Waiting for the HX711...");
-    delay(500);
-  }
-
   // Tare to zero the scale (remove any load)
   scale.tare();
   Serial.println("Scale tared.");
-
   // Set your own calibration factor here
   // current_factor * (actual weight / a bunch of factors)
   scale.set_scale(2280.0*(4980/1800)/5*.97);
 }
 
 void loop() {
-  // Only print if scale is ready
-  if (scale.is_ready()) {
-    float weight = scale.get_units(1); // Average of 5 readings
-    Serial.print("Weight: ");
-    Serial.print(weight, 3);  // Show two decimal places
-    Serial.println(" grams");
-  } else {
-    Serial.println("HX711 not found.");
-  }
-
+  float weight = scale.get_units(1); // Average of 1 readings
+  Serial.print("Weight: ");
+  Serial.print(weight, 3);  // Show three decimal places
+  Serial.println(" grams");
   delay(0.001);
 }
